dc2dbfd6b57205aa3b0b3d05bd1f1f85086e0a84
jeffchao98 <philadelphiafs@gmail.com>
2019-11-28 21:31:13 +0900

    FIX #21 - After optimize - Modified the CurrencyInfo data class for fit Room - Prepare the Dao for map the DB script and the class for setup the Room DB

---
+++ app/src/main/java/com/scchao/currencytable/data/repository/CurrencyDataRepository.kt.kt
@@ -0,0 +1,17 @@
+package com.scchao.currencytable.data.repository
+
+import com.scchao.currencytable.data.model.CurrencyInfo
+import com.scchao.currencytable.data.room.RateDao
+import com.scchao.currencytable.data.room.RateDatabase
+import org.koin.dsl.module
+
+val currencyDataRepoModule = module {
+    factory { CurrencyDataRepository(get()) }
+    factory { get<RateDatabase>().rateDao() }
+}
+
+class CurrencyDataRepository(private val rateDao: RateDao) {
+    suspend fun loadRateList(): List<CurrencyInfo> = rateDao.getAllList()
+    suspend fun updateRate(saveRate: CurrencyInfo) = rateDao.updateRate(saveRate)
+    suspend fun findRate(code: String) = rateDao.findRate(code)
+}
